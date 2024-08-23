615e23ac4a7d64f5ae02fdfb1802d4af2c1c9881
K1ngkp <kausikpattnaik24@gmail.com>
2024-08-23 16:20:49 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   
   return (
-    <nav className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-4">
+    <nav className="bg-gradient-to-r from-indigo-500 p-4">
       <div className="container mx-auto flex justify-between">
-        <h1 className="text-white text-2xl">Stock Trading Platform</h1>
+        <h1 className="text-white text-2xl font-bold">Stock Trading Platform</h1>
         <div className="flex space-x-4">
           <Link to="/" className="text-white"><button className="bg-black ">Home</button></Link>
           <Link to="/stock-details" className="text-white">Stock Details</Link>
