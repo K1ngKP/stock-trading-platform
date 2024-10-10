076a6976ac4afe7b137e69a56b6457879017ffd8
K1ngkp <kausikpattnaik24@gmail.com>
2024-10-10 19:49:07 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,12 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between">
         <h1 className="text-white text-2xl font-bold">Stock Trading Platform</h1>
         <div className="flex space-x-4">
-          <Link to="/" className="text-white"><button className="bg-black ">Home</button></Link>
-          <Link to="/stock-details" className="text-white">Stock Details</Link>
+          <Link to="/" className="text-white"><button class="bg-blue-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full w-20">
+  Home
+</button></Link>
+          <Link to="/stock-details" className="text-white"><button class="bg-blue-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full w-20">
+    Stocks
+</button></Link>
         </div>
       </div>
     </nav>
